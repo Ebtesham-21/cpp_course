ddc6cb6bc6c842d84c2172ac9fae86a8e7e31bf1
Ebtesham Hossain <ebteshamhossain98@gmail.com>
2023-01-26 14:20:27 +0000

    challenge_1

--- .ccls-cache/@home@runner@c/main.cpp
+++ .ccls-cache/@home@runner@c/main.cpp
@@ -5,7 +5,9 @@ int main()
  int favorite_number; 
  std::cout << "Enter your favorite number between 1 and 100 : ";
  std::cin >> favorite_number;
-  std::cout << "That's my num too" <<std::endl;
+  std::cout << "Amazing that's my favoraite number" <<std::endl;
+    std::cout << "Not really  " << favorite_number <<   "  is my favoraite number" <<std::endl;
+  
 
   return 0;
 }
